050f10497963f9f1243eab8aaea6669ae6f59e19
eduardo <eduardo@studos.com.br>
2020-06-13 10:48:26 -0300

    update

--- src/containers/Topbar/Topbar.jsx
+++ src/containers/Topbar/Topbar.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { ShoppingBag, Search } from 'react-feather';
 
 import './_styles.scss';
-import Sidebar from "../../components/sidebar/sidebar";
 
 const TopBar = () => {
   return (
@@ -15,14 +14,7 @@ const TopBar = () => {
           <img className="topbar__center-logo" src="//cdn.shopify.com/s/files/1/2722/8634/t/43/assets/main-logo2.svg?v=4768978662345202764" alt="Lavish Alice" />
         </Link>
         <div className="cell topbar__right">
-          <Sidebar
-            title="Carrinho de compras"
-            icon={<ShoppingBag className="topbar__right-icon-bag"  size={22} />}
-          />
-          <Sidebar
-            title="Buscar itens"
-            icon={<Search size={22} />}
-          />
+        
         </div>
       </div>
     </header>
